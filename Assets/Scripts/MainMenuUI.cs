aa621803c047d09e54b7e21de2bce67a47cd5841
Shreyansh911 <47926870+Shreyansh911@users.noreply.github.com>
2022-02-28 18:44:08 +0530

    Add files via upload

---
+++ Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] Text _startButton, _exitButton, _titleText, _level1, _level2, _level3, _level4, _level5, _endless;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ActivatingLevels();
+    }
+
+    public void StartGame()
+    {
+        _startButton.gameObject.SetActive(false);
+        _exitButton.gameObject.SetActive(false);
+        _titleText.gameObject.SetActive(false);
+
+        DeactivatingLevels();
+
+    }
+
+    private void DeactivatingLevels()
+    {
+        _level1.gameObject.SetActive(true);
+        _level2.gameObject.SetActive(true);
+        _level3.gameObject.SetActive(true);
+        _level4.gameObject.SetActive(true);
+        _level5.gameObject.SetActive(true);
+        _endless.gameObject.SetActive(true);
+    }
+
+    private void ActivatingLevels()
+    {
+        _level1.gameObject.SetActive(false);
+        _level2.gameObject.SetActive(false);
+        _level3.gameObject.SetActive(false);
+        _level4.gameObject.SetActive(false);
+        _level5.gameObject.SetActive(false);
+        _endless.gameObject.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void LoadLevel4()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void LoadLevel5()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void Endless()
+    {
+        SceneManager.LoadScene(6);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float _speed = 100;
+    [SerializeField] Text _gameoverText, _restartButton, _mainMenuButton;
+    
+
+    Vector3 _Direction = Vector3.right;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+
+        _gameoverText.gameObject.SetActive(false);
+        _restartButton.gameObject.SetActive(false);
+        _mainMenuButton.gameObject.SetActive(false);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MobileMovement();
+
+        PcMovement();
+    }
+
+    public void SpeedUp()
+    {
+        _speed++;
+    }
+
+    void PcMovement()
+    {
+        transform.Translate(_Direction * Time.deltaTime * _speed);
+
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            _Direction = Vector3.up;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            _Direction = Vector3.left;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            _Direction = Vector3.down;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            _Direction = Vector3.right;
+        }
+    }
+
+    void MobileMovement()
+    {
+        // For Player Movement.
+        transform.Translate(_Direction * Time.deltaTime * _speed);
+    }
+
+    // If Player Wants to go Up.
+    public void MoveUp()
+    {
+        _Direction = Vector3.up;
+    }
+
+    // If Player Wants to go Down.
+    public void MoveDown()
+    {
+        _Direction = Vector3.down;
+    }
+
+    // If Player Wants to go Right.
+    public void MoveRight()
+    {
+        _Direction = Vector3.right;
+    }
+
+    // If Player Wants to go Left.
+    public void MoveLeft()
+    {
+        _Direction = Vector3.left;
+    }
+
+    // When Player Collide with Border
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Wall")
+        {
+            Time.timeScale = 0;
+            _gameoverText.gameObject.SetActive(true);
+            _restartButton.gameObject.SetActive(true);
+            _mainMenuButton.gameObject.SetActive(true);
+        }
+    }
+}
